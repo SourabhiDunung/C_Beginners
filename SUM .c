2934935f36cc741cabd14bbf44de32399877ee04
Sourabhi <128148679+SourabhiDunung@users.noreply.github.com>
2023-08-03 23:00:10 +0530

    Add files via upload

---
+++ SUM .c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+	float a,b,sum;
+	printf("Enter Value of 1st number:- ");
+	scanf("%f",&a);
+	printf("Enter value of 2nd number:- ");
+	scanf("%f",&b);
+	sum=a+b;
+	printf("%f + %f = %f",a,b,sum);
+	return 0;
+}
